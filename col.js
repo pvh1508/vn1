6a3efeb8fe831081a83058bea166e33593d139b9
pvh1508 <61488485+pvh1508@users.noreply.github.com>
2020-03-18 19:57:22 +0700

    Add files via upload

--- col.js
+++ col.js
@@ -1,13 +1,4 @@
-//function myFunction1(){
-  //  document.getElementById("o1").classList.toggle("newClassName");
- //   document.getElementById("o2").classList.toggle("newClassName");
- // }
-$(document).ready(function(){
-    $(".hamburger1").click(function(){
-      $(".wrapper").toggleClass("active")
-    })
-});
-//
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
